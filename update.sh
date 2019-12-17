b4afab3fc5eb2928735ecdcb2b971dd767eca779
syto203 <26351342+syto203@users.noreply.github.com>
2019-12-17 04:56:38 +0200

    Updated Packages on 12/17/19 04:56:38

--- update.sh
+++ update.sh
@@ -1,1 +1,3 @@
-rm Packages*;dpkg-scanpackages -m debs/ /dev/null > Packages && bzip2 -c9 Packages > Packages.bz2 && gzip -c9 Packages > Packages.gz && currentDate=`date +"%D %T"` && git add . && git commit -a -S -m "Updated Packages on ${currentDate}" && git push;
+#!/bin/bash
+
+rm Packages*;dpkg-scanpackages -m debs/ /dev/null > Packages && bzip2 -c9 Packages > Packages.bz2 && gzip -c9 Packages > Packages.gz && currentDate=`date +"%D %T"` && git add . && git commit -a -S -m "Updated Packages on ${currentDate}" && git push && exit;
